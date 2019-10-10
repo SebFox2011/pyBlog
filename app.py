48b8eeb6d8bb94a8c269291512cd206ca7f0674b
Seb <sebastien.philippe5@gmail.com>
2019-10-10 22:14:55 +0200

    Ajouter des vues edit Form et Message ainsi que les vues d'édition

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def blog():
     posts = Post.query.all()
     comments = Comment.query.all()
     return render_template('blog.html', posts=posts, comments=comments)
-    #return render_template('blog.html')
+
 
 @app.route('/blog/post/add', methods=["GET", "POST"])
 def addPost():
@@ -46,13 +46,27 @@ def addPost():
         return redirect(url_for('blog'))
 
 
-@app.route('/blog/post/edit')
-def editPost():
-    return render_template('index.html')
+@app.route('/blog/post/edit/<id>', methods=["GET", "POST"])
+def editPost(id):
+    # Récupération du post
+    post = Post.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        return render_template('editPostForm.html', post=post)
+    else:
+        # Récupération de l'article
+        post.update(request.form)
+
+        return redirect(url_for('blog'))
 
 
-@app.route('/blog/post/delete')
-def deletePost():
+@app.route('/blog/post/delete/<id>')
+def deletePost(id):
+    post = Post.query.filter_by(id=id).first()
+    ##comment = Comment.query.filter_(post_id=id).all()
+    db.session.delete(post)
+    ##db.session.delete(comment)
+    db.session.commit()
     return render_template('index.html')
 
 
@@ -66,20 +80,32 @@ def addComment(id):
         name = request.form['messageName']
         content = request.form['messageContent']
         # Création du post
-        comment = Comment(name=name, content=content,post_id=id)
+        comment = Comment(name=name, content=content, post_id=id)
         db.session.add(comment)
         db.session.commit()
 
         return redirect(url_for('blog'))
 
 
-@app.route('/blog/comment/edit')
-def editComment():
-    return render_template('index.html')
+@app.route('/blog/comment/edit/<id>', methods=["GET", "POST"])
+def editComment(id):
+    # Récupération du post
+    comment = Comment.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        return render_template('editMessageForm.html', comment=comment)
+    else:
+        # Récupération de l'article
+        comment.update(request.form)
+
+        return redirect(url_for('blog'))
 
 
-@app.route('/blog/comment/delete')
-def deleteComment():
+@app.route('/blog/comment/delete/<id>')
+def deleteComment(id):
+    comment = Comment.query.filter_by(id=id).first()
+    db.session.delete(comment)
+    db.session.commit()
     return render_template('index.html')
 
 
--- migrations/versions/84ede090d75d_.py
+++ migrations/versions/84ede090d75d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 781857f6a47d
+Revision ID: 84ede090d75d
 Revises: 
-Create Date: 2019-10-10 14:15:11.433452
+Create Date: 2019-10-10 21:29:14.155735
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '781857f6a47d'
+revision = '84ede090d75d'
 down_revision = None
 branch_labels = None
 depends_on = None
